1a9f21a601c5f5264a41ab3b8ef87a656691561c
Shiming Zhang <wzshiming@foxmail.com>
2023-11-05 22:08:17 +0800

    Fix GH Clear Date

--- github/github-clear-date.user.js
+++ github/github-clear-date.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Github Clear Date
 // @namespace   https://github.com/wzshiming/userscripts
-// @version     0.1
+// @version     0.2
 // @description Add a clear date to the relative time in Github
 // @author      wzshiming
 // @match       *://github.com/*
@@ -21,14 +21,19 @@
         window.MozMutationObserver;
 
     let formatTime = function (item) {
-        let text = item.innerText;
+        let text = item.shadowRoot.innerHTML;
         if (text.length == 0 || text.indexOf("(") >= 0) {
             return
         }
-        let tmp = text.toLowerCase();
-        if (tmp.indexOf("day") >= 0 || tmp.indexOf("month") >= 0 || tmp.indexOf("year") >= 0) {
-            item.innerText = item.date.toISOString().split("T")[0].replaceAll("-", "/") + "(" + text + ")";
+        let date = item.datetime.split("T")[0].replaceAll("-", "/")
+        if (date.length < 8) {
+            return
         }
+
+        if (date[0] == "2" && date[1] == "0") {
+            date = date.substr(2);
+        }
+        item.shadowRoot.innerHTML += "(" + date + ")";
     }
 
     let mutation = function (mutationsList, observer) {
@@ -42,7 +47,7 @@
         if (!elem) {
             return
         }
-        elem.querySelectorAll("[datetime]").forEach(formatTime);
+        elem.querySelectorAll('relative-time').forEach(formatTime);
     }
     let config = { childList: true, subtree: true, characterData: true };
     let ids = ["dashboard", "js-repo-pjax-container", "js-pjax-container"]

534118de7127f561d7939096b0b3c5956dc4cc21
Shiming Zhang <wzshiming@foxmail.com>
2023-11-07 17:06:12 +0800

    Short the year

--- github/github-clear-date.user.js
+++ github/github-clear-date.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Github Clear Date
 // @namespace   https://github.com/wzshiming/userscripts
-// @version     0.2
+// @version     0.3.0
 // @description Add a clear date to the relative time in Github
 // @author      wzshiming
 // @match       *://github.com/*
@@ -20,46 +20,50 @@
         window.WebKitMutationObserver ||
         window.MozMutationObserver;
 
-    let formatTime = function (item) {
-        let text = item.shadowRoot.innerHTML;
-        if (text.length == 0 || text.indexOf("(") >= 0) {
-            return
-        }
-        let date = item.datetime.split("T")[0].replaceAll("-", "/")
-        if (date.length < 8) {
-            return
-        }
-
-        if (date[0] == "2" && date[1] == "0") {
-            date = date.substr(2);
-        }
-        item.shadowRoot.innerHTML += "(" + date + ")";
-    }
-
-    let mutation = function (mutationsList, observer) {
-        setTimeout(function () {
-            for (let mutation of mutationsList) {
-                mutate(mutation.target.parentNode);
-            }
-        }, Math.round(Math.random() * 1000));
-    }
-    let mutate = function (elem) {
-        if (!elem) {
-            return
-        }
-        elem.querySelectorAll('relative-time').forEach(formatTime);
-    }
-    let config = { childList: true, subtree: true, characterData: true };
-    let ids = ["dashboard", "js-repo-pjax-container", "js-pjax-container"]
-    for (let id of ids) {
-        let elem = document.getElementById(id);
-        if (!elem) {
-            continue
-        }
-        let observer = new MutationObserver(mutation);
-        observer.observe(elem, config);
-        break
-    }
-
-    mutate(document);
+    let config = {
+        childList: true,
+        subtree: true,
+        attributes: true,
+    };
+    let observer = new MutationObserver(mutationForce);
+    observer.observe(document.body, config);
+    mutate(document.body);
 })();
+
+function mutate(elem) {
+    elem.querySelectorAll('relative-time').forEach(formatTime);
+}
+
+function mutationForce(mutationsList, observer) {
+    mutation(mutationsList);
+    setTimeout(function () {
+        mutation(mutationsList);
+    }, 1000 + Math.round(Math.random() * 1000));
+}
+
+function mutation(mutationsList) {
+    for (let mutation of mutationsList) {
+        mutate(mutation.target.parentNode);
+    }
+}
+
+function formatTime(item) {
+    let text = item.shadowRoot.innerHTML;
+    if (text.length == 0 || text.indexOf("(") >= 0) {
+        return
+    }
+    let date = item.datetime.split("T")[0].replaceAll("-", "/")
+    if (date.length < 8) {
+        return
+    }
+
+    let now = new Date();
+    let year = now.getFullYear();
+    if (date.indexOf(year) == 0) {
+        date = date.substr(5);
+    } else if (date[0] == "2" && date[1] == "0") {
+        date = date.substr(2);
+    }
+
+    item.shadowRoot.innerHTML += "(" + date + ")";
+}
